8da648adc8eeefea540f9acba4698d291d9d6f57
anuprovo-debnath <166212030+anuprovo-debnath@users.noreply.github.com>
2026-04-07 01:51:08 +0530

    jekyll

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   basePath: '/slashdot-website-2026',
   assetPrefix: '/slashdot-website-2026/',
 };
