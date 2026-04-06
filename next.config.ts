62c36a5aa7a0e3b1cda124188dab0a6983c24579
anuprovo-debnath <166212030+anuprovo-debnath@users.noreply.github.com>
2026-04-07 01:36:28 +0530

    Updating paths

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // Note: if you are deploying to a domain like https://username.github.io/slashdot-website-2026/
-  // you may need to uncomment and set the basePath below:
-  // basePath: '/slashdot-website-2026',
+  basePath: '/slashdot-website-2026',
+  assetPrefix: '/slashdot-website-2026/',
 };
 
 export default nextConfig;
