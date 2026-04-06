aaeec302516c58aa4af013620f7733f528ce3cd7
anuprovo-debnath <166212030+anuprovo-debnath@users.noreply.github.com>
2026-04-07 01:22:00 +0530

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Note: if you are deploying to a domain like https://username.github.io/slashdot-website-2026/
+  // you may need to uncomment and set the basePath below:
+  // basePath: '/slashdot-website-2026',
 };
 
 export default nextConfig;
