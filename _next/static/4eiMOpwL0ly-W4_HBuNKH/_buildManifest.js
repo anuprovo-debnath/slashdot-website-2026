e55ff7437fae25b410b696853b76407c3c5e080a
anuprovo-debnath <anuprovo-debnath@users.noreply.github.com>
2026-04-06 20:06:57 +0000

    deploy: 62c36a5aa7a0e3b1cda124188dab0a6983c24579

--- _next/static/4eiMOpwL0ly-W4_HBuNKH/_buildManifest.js
+++ _next/static/4eiMOpwL0ly-W4_HBuNKH/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/slashdot-website-2026//_next/:path+",
+        "destination": "/slashdot-website-2026/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
